2dc887e3fca5c85671d889daf512c6921afa5a7c
GIANBOT12 <100688757+GIANBOT12@users.noreply.github.com>
2022-03-17 19:34:19 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,26 +13,26 @@ global.APIKeys = {
 
 // setting 
 global.autoread = false // auto read pesan / message
-global.autorecording = true //status auto merekam ( auto record )
+global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 
 // Other
 global.botname = "Alphabot-Mdོ" //namabot kalian
 global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api-alphabot.herokuapp.com/" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.myweb ="https://herokuapp.com" //bebas asal jan hapus
+global.youtube = "https://Wibusoft.com" //bebas asal jan hapus
+global.github = "https://github.com" //bebas
+global.email = "giangans329@gmail.com" //bebas
 global.region = "Indonesia" //bebas
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.ownernomer = "6282363833003" // nomor wa kalian
+global.ownernomerr = "+6282363833003" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["62887435047326","62887435047326","6285342106390"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.owner = ["62887435047326","6282363833003","6283186871450"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© Ciaabot' //sticker wm ubah
+global.author = 'Di buat oleh Ciaabot' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
