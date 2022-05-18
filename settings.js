e71059510de5860546e2c2c33b3d812628c6ff63
GIANBOT12 <100688757+GIANBOT12@users.noreply.github.com>
2022-05-18 08:49:31 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,8 +18,8 @@ global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 
 // Other
-global.botname = "Gian ❤️ Lia" //namabot kalian
-global.ownername= "lia ×፝֟͜×" //nama kalian
+global.botname = "GiannnBots" //namabot kalian
+global.ownername= "Giann ×፝֟͜×" //nama kalian
 global.myweb ="https://herokuapp.com" //bebas asal jan hapus
 global.youtube = "https://Wibusoft.com" //bebas asal jan hapus
 global.github = "https://github.com" //bebas
@@ -31,8 +31,8 @@ global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder i
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.owner = ["6287889887883","6287889887883","6287889887883"] //ganti agar fitur owner bisa di gunakan
-global.packname = '©LiaGian' //sticker wm ubah
-global.author = 'Di buat oleh Liagian' //sticker wm ganti nama kalian
+global.packname = '©Gian' //sticker wm ubah
+global.author = 'Di buat oleh gian' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
